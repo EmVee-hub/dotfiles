9aeda1ac3bc18c0825d27cd452db8000a99037b0
Emvee <martinsvitols710@gmail.com>
2026-06-17 09:47:46 +0300

    feat(setup): init bin setup

--- setup.sh
+++ setup.sh
@@ -2,10 +2,20 @@
 # setup.sh
 
 DOTFILES_DIR="$(cd "$(dirname "$(realpath "$0")")" && pwd)"
+BIN_DIR="$DOTFILES_DIR/bin"
 
 find "$DOTFILES_DIR/shell" -maxdepth 1 -type f | while read -r source; do
     filename="${source##*/}"
     target="$HOME/.$filename"
+
+    echo "Creating symlink: $target -> $source"
+    ln -sfn "$source" "$target"
+    chmod +x "$source"
+done
+
+find "$DOTFILES_DIR/bin" -maxdepth 1 -type f | while read -r source; do
+    filename="${source##*/}"
+    target="$HOME/.local/bin/$filename"
 
     echo "Creating symlink: $target -> $source"
     ln -sfn "$source" "$target"
